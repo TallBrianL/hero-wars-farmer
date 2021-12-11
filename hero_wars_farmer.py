11e1cb0d0a246f0a2ef77a6a5100f0720c87ab98
TallBrian <brian.lause@gmail.com>
2021-12-11 11:55:23 -0600

    feature - detect end of campaign battles refactor - extract method apply offset for clarity - divide campaign and tower logic for speed - in tower take one screenshot for level detection instead of retaking for each level type for correctness - cleanup warnings

--- hero_wars_farmer.py
+++ hero_wars_farmer.py
@@ -1,3 +1,4 @@
+import glob
 import json
 import time
 
@@ -21,55 +22,83 @@ class HeroWarsFarmer:
             self.controls = list_to_dict(coordinates["general"])
             self.cities = list_to_dict(coordinates["cities"])
 
-    def move_and_click_offset(self, item, my_time):
+    def apply_offset(self, pos):
+        return pos[0] + self.offset[0], pos[1] + self.offset[1]
+
+    def click_item(self, item, my_time):
         pos = item['position']
-        offset_pos = (pos[0] + self.offset[0], pos[1] + self.offset[1])
-        self.move_and_click(offset_pos, my_time)
+        self.click_location(self.apply_offset(pos), my_time)
 
     @staticmethod
-    def move_and_click(loc, my_time):
+    def click_location(loc, my_time):
         pyautogui.moveTo(*loc, duration=my_time)
         time.sleep(0.5)
         pyautogui.click(*loc)
 
-    def get_current_chapter(self):
-        pos = (302, 189)
-        offset_pos = (pos[0] + self.offset[0], pos[1] + self.offset[1])
-        span = (708, 222)
-        return self.ocr_chapter(offset_pos, span)
+    @staticmethod
+    def locate_file_any_suffix(needle, haystack):
+        for f in glob.glob('screenshots\\' + needle + "*.PNG"):
+            loc = pyautogui.locate(f, haystack)
+            if loc:
+                return pyautogui.center(loc)
+        return None
 
     @staticmethod
-    def ocr_chapter(pos, span):
+    def locate_try_iters(image, max_iters=10):
+        loc = None
+        count = 0
+        while not loc and count < max_iters:
+            loc = pyautogui.locateCenterOnScreen('screenshots\\' + image + '.PNG')
+            time.sleep(0.1)
+            count += 1
+        if not loc:
+            raise RuntimeError("Could not find " + image)
+        return loc
+
+    @staticmethod
+    def detect_and_push(button, max_iters=10):
+        loc = HeroWarsFarmer.locate_try_iters(button, max_iters)
+        HeroWarsFarmer.click_location(loc, 0.2)
+
+    @staticmethod
+    def ocr_screen(pos, span):
         time.sleep(0.5)
         temp = pyautogui.screenshot()
         cropped = temp.crop((*pos, *span))
         my_string = pytesseract.image_to_string(cropped)
-        words = my_string.split()
-        assert(words[0] == "Chapter")
-        return int(words[1][:-1])
+        return my_string.split()
 
+    # --------------------------------------------------
+    # CAMPAIGN RUNNER
     def enter_campaign(self):
-        self.move_and_click_offset(self.controls["enterCampaign"], 1)
+        self.click_item(self.controls["enterCampaign"], 1)
         self.current_chapter = self.get_current_chapter()
+
+    def get_current_chapter(self):
+        pos = (302, 189)
+        span = (708, 222)
+        my_string = self.ocr_screen(self.apply_offset(pos), span)
+        words = my_string.split()
+        assert (words[0] == "Chapter")
+        return int(words[1][:-1])
 
     def go_to_chapter(self, chapter):
         while chapter < self.current_chapter:
-            self.move_and_click_offset(self.controls["chapter_down"], .2)
+            self.click_item(self.controls["chapter_down"], 0.2)
             time.sleep(0.5)
             self.current_chapter -= 1
         while chapter > self.current_chapter:
-            self.move_and_click_offset(self.controls["chapter_up"], .2)
+            self.click_item(self.controls["chapter_up"], 0.2)
             time.sleep(0.5)
             self.current_chapter += 1
 
-    def run_level(self, city, my_time=DEFAULT_TIME):
+    def run_campaign_level(self, city):
         self.go_to_chapter(city["chapter"])
-        self.move_and_click_offset(city, .5)
-        self.move_and_click_offset(self.controls["start"], .2)
-        self.move_and_click_offset(self.controls["battle"], .2)
-        self.move_and_click_offset(self.controls["auto"], 5.5)
-        wait(my_time)
-        self.move_and_click_offset(self.controls["cont"], 1)
+        self.click_item(city, .5)
+        self.click_item(self.controls["start"], 0.2)
+        HeroWarsFarmer.detect_and_push('to_battle')
+        HeroWarsFarmer.detect_and_push('auto_off', 100)
+        HeroWarsFarmer.detect_battle_complete()
 
     def select_cities(self, loot_desired, max_level):
         cities = []
@@ -87,110 +116,125 @@ class HeroWarsFarmer:
     def farm_cities(self, farm_list):
         for [city, quantity] in farm_list:
             for _ in range(quantity):
-                self.run_level(city)
+                self.run_campaign_level(city)
 
+    # --------------------------------------------------
+    # TOWER RUNNER
     def run_tower(self):
+        print('running tower')
         while True:
             self.run_tower_floor()
+            print('sleeping for 2')
+            time.sleep(2)
 
     def run_tower_floor(self):
-        is_level_identified = False
-        for _ in range(10):
-            if pyautogui.locateCenterOnScreen('screenshots\\tower_to_battle.PNG'):
+        print('detecting floor')
+        count = 0
+        loc = None
+
+        while count < 10:
+            print('detecting...')
+            floor_screen_capture = pyautogui.screenshot()
+
+            loc = self.locate_file_any_suffix('tower_to_battle', floor_screen_capture)
+            if loc:
                 print('battle detected')
-                self.run_battle_floor()
-                is_level_identified = True
-            elif pyautogui.locateCenterOnScreen('screenshots\\tower_treasure.PNG'):
-                self.run_treasure_floor()
-                is_level_identified = True
-            elif pyautogui.locateCenterOnScreen('screenshots\\tower_buff_chest.PNG'):
-                self.run_buff_floor()
-                is_level_identified = True
-            elif pyautogui.locateCenterOnScreen('screenshots\\tower_buff_chest2.PNG'):
-                self.run_buff_floor()
-                is_level_identified = True
-            elif pyautogui.locateCenterOnScreen('screenshots\\tower_treasure2.PNG'):
-                self.run_treasure_floor()
-                is_level_identified = True
-        if is_level_identified:
-            time.sleep(1)
+                self.run_battle_floor(loc)
+                break
+
+            loc = self.locate_file_any_suffix('tower_treasure', floor_screen_capture)
+            if loc:
+                print('treasure detected')
+                self.run_treasure_floor(loc)
+                break
+
+            loc = self.locate_file_any_suffix('tower_buff', floor_screen_capture)
+            if loc:
+                print('buff chest detected')
+                self.run_buff_floor(loc)
+                break
+
+            count += 1
+
+        if loc:
             return 0
         else:
             raise ValueError("Could not find tower floor type")
 
     @staticmethod
-    def push(button, max_iters=10):
-        loc = None
-        count = 0
-        while not loc and count < max_iters:
-            loc = pyautogui.locateCenterOnScreen('screenshots\\' + button + '.PNG')
-            time.sleep(0.1)
-            count += 1
-        if not loc:
-            raise RuntimeError("Could not find " + button + " button")
-        HeroWarsFarmer.move_and_click(loc, 0.2)
+    def run_battle_floor(loc=None):
+        print('running battle floor')
+        if loc:
+            HeroWarsFarmer.click_location(loc, 0.2)
+        else:
+            HeroWarsFarmer.detect_and_push('tower_to_battle')
+        try:
+            HeroWarsFarmer.detect_and_push('tower_battle_skip')
+            return 0
+        except RuntimeError:
+            print('could not skip')
+        HeroWarsFarmer.detect_and_push('tower_battle_attack')
+        HeroWarsFarmer.detect_and_push('to_battle')
+        HeroWarsFarmer.detect_and_push('auto_off', 100)
+        HeroWarsFarmer.detect_battle_complete()
 
     @staticmethod
-    def run_battle_floor():
-        HeroWarsFarmer.push('tower_to_battle')
-        try:
-            HeroWarsFarmer.push('tower_battle_skip')
-        except RuntimeError:
-            HeroWarsFarmer.push('tower_battle_attack')
-        HeroWarsFarmer.push('to_battle')
-        HeroWarsFarmer.push('auto_off', 100)
+    def detect_battle_complete():
         t = 0
         while t < 180:
-            if pyautogui.locateCenterOnScreen('screenshots\\victory.PNG'):
+            floor_screen_capture = pyautogui.screenshot()
+            loc = HeroWarsFarmer.locate_file_any_suffix('victory', floor_screen_capture)
+            if loc:
                 print('Victory!')
-                HeroWarsFarmer.push('ok')
+                HeroWarsFarmer.detect_and_push('continue')
                 return 0
-            elif pyautogui.locateCenterOnScreen('screenshots\\defeat.PNG'):
+            loc = HeroWarsFarmer.locate_file_any_suffix('defeat', floor_screen_capture)
+            if loc:
                 print('Defeat :(')
-                HeroWarsFarmer.push('ok')
+                HeroWarsFarmer.detect_and_push('continue')
                 return 1
+
             time.sleep(0.5)
             t += 0.5
+            print('.', end='')
         raise RuntimeError("Battle End Not Detected, Timeout")
 
-    def run_buff_floor(self):
+    def run_buff_floor(self, loc=None):
+        print('running buff floor')
+        if loc:
+            HeroWarsFarmer.click_location(loc, 0.2)
+        else:
+            try:
+                self.detect_and_push('tower_buff_chest')
+            except RuntimeError:
+                self.detect_and_push('tower_buff_chest2')
+        self.click_item(self.controls["tower_buff_close"], 0.2)
         try:
-            self.push('tower_buff_chest')
+            HeroWarsFarmer.detect_and_push('tower_proceed')
         except RuntimeError:
-            self.push('tower_buff_chest2')
-        self.move_and_click_offset(self.controls["tower_buff_close"], 0.2)
-        try:
-            HeroWarsFarmer.push('tower_proceed')
-        except RuntimeError:
-            HeroWarsFarmer.push('tower_proceed2')
+            HeroWarsFarmer.detect_and_push('tower_proceed2')
         return 0
 
     @staticmethod
-    def run_treasure_floor():
-        try:
-            HeroWarsFarmer.push('tower_treasure')
-        except RuntimeError:
-            HeroWarsFarmer.push('tower_treasure2')
-        HeroWarsFarmer.push('tower_treasure_open')
-        HeroWarsFarmer.push('proceed')
+    def run_treasure_floor(loc=None):
+        print('running treasure floor')
+        if loc:
+            HeroWarsFarmer.click_location(loc, 0.2)
+        else:
+            try:
+                HeroWarsFarmer.detect_and_push('tower_treasure')
+            except RuntimeError:
+                HeroWarsFarmer.detect_and_push('tower_treasure2')
+        HeroWarsFarmer.detect_and_push('tower_treasure_open')
+        HeroWarsFarmer.detect_and_push('proceed')
         return 0
+
 
 def list_to_dict(item_list: list):
     named_dict = {}
     for value in item_list:
         named_dict[value["name"]] = value
     return named_dict
-
-
-def wait(my_time):
-    print('00', end='')
-    for _ in range(my_time):
-        time.sleep(1)
-        print('.', end='')
-        if not (_ + 1) % 10:
-            for _2 in range(12):
-                print('\b', end='')
-            print(_ + 1, end='')
 
 
 if __name__ == '__main__':
